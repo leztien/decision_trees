b9ae9d6d5a36797245a7b72cba21ee839a0f9de5
I.N <48551497+leztien@users.noreply.github.com>
2020-03-14 18:05:01 +0100

    Update decision_tree_for_discrete_data.py

--- decision_tree_for_discrete_data.py
+++ decision_tree_for_discrete_data.py
@@ -1,64 +1,36 @@
+
+
 """
-Decision Tree Classifier for binary data
+Data-Maker of discreete multiclass data for
+Decision Tree for discreet multiclass data (BUG)
 """
 
-def make_data(m=100, n=3, make_identical_observations_consistent=False, balanced=True, seed=None):
+
+def make_data(m=1000, n=5, k=3, seed=None):
     import numpy as np
-    """make binary data for a binary Decision Tree Classifier"""
+    """make discreet data for a multi-class Decision Tree Classifier"""
     #random seed
     if seed is True:
         seed = __import__("random").randint(0, 1000)
         print("seed =", seed)
     if seed:
         np.random.seed(int(seed))
-
+        
     #make data
-    pp = np.random.random(size=n)  # proportions of True in each feature
-    X = np.random.binomial(n=1, p=pp, size=(m,n))
-    Xoriginal = X.copy().astype('float')  # Xoriginal will be returned; mangle the X
-
-    #reverse the boolean values of some features (e.g. is_old >> is_new)
-    def func(X):
-        mask = np.random.binomial(n=1, p=0.25, size=n).astype("uint8")
-        ff = np.array([np.vectorize(lambda x:x), np.logical_not])[mask]
-        Xbool = X.astype("bool")
-        Xnew = np.empty_like(X, dtype='bool')
-        for i,row in enumerate(Xbool):
-            Xnew[i] = [f(x) for f,x in zip(ff,row)]
-        return Xnew.astype('uint8')
-
-    X = func(X)
+    df = 5
+    max_values = np.ceil(np.random.chisquare(df=df, size=n)).astype("uint16")
+    pp = np.random.uniform(low=0, high=1, size=len(max_values))
+    X = np.random.binomial(n=max_values, p=pp, size=(m, len(max_values)))
+    weights = np.random.uniform(low=0, high=1, size=n)
+    weights **= n+k
+    negative_weights = np.random.choice([-1,1], size=n, replace=True)
+    weights *= negative_weights
+    y = ((X+1)*weights).sum(axis=1)
+    bins = np.quantile(y, q=np.linspace(0,1, num=k+1)[1:-1])
+    y = np.digitize(y, bins=bins)
+    return(X,y)
 
 
-    
-    #f!ck up a certain proportion of each feature by reversing the boolean value in a coresponding cell
-    high = (1/n)**2  # maximum allowed proportion to be mangled (the lower n the higher this proportion)
-    pp = np.random.uniform(low=0, high=high, size=n)  # proportion of each feature to be mangled
-    g = (np.random.permutation(m)[:int(m*p)] for p in pp)
-    for j,nx in enumerate(g):
-        X[nx,j] = np.logical_not(X[nx,j])  # reverse the boolean feature in certain cells
-
-    #compute the target
-    y = X.sum(axis=1)
-
-    #balanced or imbalanced dataset?
-    lower = abs(float(balanced)) if isinstance(balanced,float) else 0.5
-    threshold = 0.5 if balanced is True else np.random.uniform(lower, 0.99)
-    q = np.quantile(y, q=threshold)
-    y = (y >= q).astype('uint8')
-    
-    if make_identical_observations_consistent:
-        def make_consistent(X,y):
-            """make an unsplittable (degenerate) dataset splittable by assigning a mode label to all identical observations"""
-            from scipy.stats import mode
-            unique_observations = frozenset(tuple(x) for x in X)
-            masks = [[tuple(x) == unique for x in X] for unique in unique_observations] 
-            for mask in masks:
-                y[mask] = mode(y[mask]).mode[0]
-            return(y)
-        y = make_consistent(Xoriginal,y)
-    #return X,y
-    return(Xoriginal,y)
 
 #######################################################################
 
@@ -67,22 +39,38 @@ import numpy as np
 
 
 """METRICS"""
-def gini(cc):
-    total = sum(cc)
-    if total==0: return 1
-    return 1 - (cc[0]/total)**2 - (cc[1]/total)**2
+def gini(counts):
+    from functools import reduce
+    from operator import sub
+    total = sum(counts)
+    seq = (1,) + tuple((count/total)**2 for count in counts)
+    ans = reduce(sub, seq)
+    return ans
 
-def Gini(nx,j, X, y):
-    xy = tuple(np.c_[X[nx,j]==1, y[nx]].astype('int8').tolist())
-    values = ([1,1],[1,0],[0,1],[0,0])
-    a,b,c,d = [xy.count(v) for v in values]
-    g1 = gini([a,b])
-    g0 = gini([c,d])
-    n1 = sum([a,b])
-    n0 = sum([c,d])
-    N = n1+n0
-    G = (n1/N)*g1 + (n0/N)*g0
-    return(G)
+
+def Gini(nx, j, value, X, y):
+    """Gini avg of an internal node"""
+    mask = X[nx,j] == value
+    if len(nx)==0:
+        from math import inf
+        return inf
+    y_false = tuple(y[nx][~mask])
+    y_true = tuple(y[nx][mask])
+    
+    assert len(y_false)+len(y_true) == len(mask)
+    
+    counts_false = [y_false.count(label) for label in set(y_false)]
+    counts_true = [y_true.count(label) for label in set(y_true)]
+    
+    gini_false = gini(counts_false)
+    gini_true = gini(counts_true)
+    
+    proportion_false = len(y_false) / len(mask)
+    proportion_true = len(y_true) / len(mask)
+    
+    ans = gini_false * proportion_false + gini_true * proportion_true
+    return ans
+
 
 def entropy(y):
     assert hasattr(y, '__iter__'), "bad argument"
@@ -94,6 +82,7 @@ def entropy(y):
     E = -sum(p * log(p) for p in pp)
     return E
 
+
 def information_gain(y,x):
     assert all(hasattr(o, '__iter__') for o in (y,x)) and len(y)==len(x), "bad arguments"
     n = len(y)
@@ -104,26 +93,28 @@ def information_gain(y,x):
     return(I)
     
 
+
 class Node:
-    def __init__(self, rule):
-        self.rule = rule
+    def __init__(self, feature, value):
+        self.feature = feature
+        self.value = value
         self.next_nodes = None
     def forward(self, x):
-        j = self.rule
-        if int(x[j])==1:
-            ans = self.next_nodes[1].forward(x)
-        elif int(x[j])==0:
+        if int(x[self.feature]) != self.value:
             ans = self.next_nodes[0].forward(x)
-        else: raise ValueError("bad value")
+        else:
+            ans = self.next_nodes[1].forward(x)
         return ans
+
 
 
 class Leaf:
     def __init__(self, predicted_class):
-        self.predicted_class = self.rule = predicted_class
+        self.predicted_class = predicted_class
     def forward(self, x):
         ypred = self.predicted_class
         return ypred
+
 
 
 class Tree:
@@ -132,6 +123,7 @@ class Tree:
         self.root = None
         self.graph = ["digraph Tree {node [shape=box];",]
         self._nodes_counter = 0
+        self._depth_counter = 0
 
     def fit(self, X,y):
         self.X, self.y = X,y
@@ -147,20 +139,24 @@ class Tree:
         ypred = [self.root.forward(x) for x in X]
         return ypred
 
-    def add_node(self, nx, previous_node_rule=None):
-        cc = [(y[nx]==k).sum() for k in self.classes]
+    def add_node(self, nx, previous_node_feature=None):
+        counts = [(y[nx]==k).sum() for k in self.classes]
         
+        """select metric"""
         if self.metric == 'gini':
-            g = gini(cc)
-            features = set(range(self.n)).difference([previous_node_rule])
-    
+            g = gini(counts)
+            features = set(range(self.n)).difference([previous_node_feature])
+            print(len(nx), counts, self._nodes_counter)
             Ginis = list()
             for j in features:
-                G = Gini(nx,j, self.X, self.y)
-                Ginis.append((j,G))
+                values = sorted(set(X[:,j]))
+                for v in values:
+                    G = Gini(nx, j, v, self.X, self.y)
+                    Ginis.append((j,v,G))
             G = sorted(Ginis, key=(lambda t:t[-1]))[0]   # best Gini
-            base_case_condition = g <= G[-1] * (1+1E-10) #  * (1+1E-10)  prevents a bug. IDK wether it is the correct solution
-        
+            base_case_condition = (g <= G[-1]*1.1) or (sum(counts) <= 2) or self._depth_counter > 10
+            #  * (1+1E-10)  prevents a bug. IDK wether it is the correct solution
+            
         else:
             g = 0
             Gains = list()
@@ -169,58 +165,72 @@ class Tree:
                 Gains.append((j,G))
             G = sorted(Gains, key=(lambda t:t[-1]))[-1]  # the best IG is at the end
             base_case_condition = G[-1] < 1E-10   # works also if ==0
+            
         
         """the recursion section"""
         #base case:
         if base_case_condition:    
-            leaf = Leaf(predicted_class=np.array(cc).argmax())
+            leaf = Leaf(predicted_class=np.array(counts).argmax())
             leaf._node_number = self._nodes_counter
             self._nodes_counter += 1
-            if self.metric == 'gini':  # graphviz
-                txt = 'gini={0:.3f}\n{1:}/{2:}\nclass {3:}'.format(g, *cc, leaf.rule)
-                self.graph.append('{} [color="black", fillcolor="green", style="filled", fontsize=10, label="{}"]'.format(leaf._node_number, txt))
+#            if self.metric == 'gini':  # graphviz
+#                txt = 'gini={0:.3f}\n{1:}/{2:}\nclass {3:}'.format(g, *counts, leaf.rule)
+#                self.graph.append('{} [color="black", fillcolor="green", style="filled", fontsize=10, label="{}"]'.format(leaf._node_number, txt))
             return leaf
         #non-base case
         else: 
-            j = G[0]
-            thisnode = Node(rule=j)
+            feature,value = G[0], G[1]
+            thisnode = Node(feature=feature, value=value)
             thisnode._node_number = self._nodes_counter
             self._nodes_counter += 1
-            next_nodes = [self.add_node(nx[self.X[nx,j]==k], previous_node_rule=thisnode.rule) for k in self.classes]
+            next_nodes = [self.add_node(nx[self.X[nx,j] != value], previous_node_feature=thisnode.feature),
+                          self.add_node(nx[self.X[nx,j] == value], previous_node_feature=thisnode.feature)]
             thisnode.next_nodes = next_nodes  # note the order - for indexing puropses
-
+            self._depth_counter += 1
+            return thisnode
             #graphviz
             if self.metric == 'gini':
                 angles = [45, -45]
                 branches = [True, False]
                 for (node,angle,branch) in zip(next_nodes, angles, branches):
                     self.graph.append('{} -> {} [labeldistance=2.5, labelangle={}, fontsize=8, headlabel="{}"]'.format(thisnode._node_number, node._node_number, angle, branch))
-                txt = 'x{} = True\nGini={:.3f}\n{}/{}'.format(thisnode.rule, G[1], *cc)
+                txt = 'x{} = True\nGini={:.3f}\n{}/{}'.format(thisnode.rule, G[1], *counts)
                 self.graph.append('{} [color="black", fillcolor="orange", style="filled", fontsize=10, label="{}"]'.format(thisnode._node_number, txt))
             return thisnode
 
 #############################################################################
+    
+    
+############################################################################################
+############################################################################################
 
-X,y = make_data(m=200, n=10, make_identical_observations_consistent=False, 
-                balanced=True, seed=True)  
 
-tree = Tree(metric='entropy').fit(X,y)
+X,y = make_data(m=1000, n=3, k=3, seed=True)
+        
+
+tree = Tree(metric='gini').fit(X,y)
 ypred = tree.predict(X)
 acc = np.equal(y, ypred).mean()
 print("accuracy =", acc, "\tnumber of nodes =", tree._nodes_counter)
 
 
-#graphviz
-string = tree.graph
-try: from graphviz import Source
-except: print("Unable to import graphviz")
-else:
-    graph = Source(string, filename="image", format="png")
-    graph.view()
+
 
 
 
 from sklearn.tree import DecisionTreeClassifier
-md = DecisionTreeClassifier().fit(X,y)
-acc = md.score(X,y)
-print("sklearn acc", acc)
+from sklearn.model_selection import train_test_split
+
+Xtrain,Xtest, ytrain,ytest = train_test_split(X,y)
+
+md = DecisionTreeClassifier(min_samples_split=5, min_samples_leaf=5).fit(Xtrain, ytrain)
+
+acc = md.score(Xtest, ytest)
+print("acc", acc)
+
+
+md.feature_importances_
+
+P = md.predict_proba(X)
+
+
